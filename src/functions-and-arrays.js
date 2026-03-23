7d11b95042896fac4a65ec5a0a8dcc797ff51f79
Laurasang13 <laurasglez@gmail.com>
2026-03-23 15:02:41 +0000

    falta un punto

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,13 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    } else if (num2 > num1) {
+        return num2;
+    } else {
+        return num1;
+    }
+}
 
 
 
@@ -7,7 +15,18 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+    if (words.length === 0) {
+        return null;
+    }
+    let longestWord = "";
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].length > longestWord.length) {
+            longestWord = words[i];
+        }
+    }
+    return longestWord;
+}
 
 
 
@@ -15,7 +34,14 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++){
+        // sum = sum + numbers[i];
+        sum += numbers[i];
+    }
+    return sum;
+}
 
 
 
@@ -23,7 +49,18 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbers2) {
+    if (numbers2.length === 0) {
+        return null;
+    }
+    let sum = 0;
+
+    for (let i= 0; i < numbers2.length; i++) {
+        sum += numbers2[i];
+    }
+    return sum / numbers2.length;
+}
+
 
 
 
@@ -31,4 +68,15 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(words2, wordtoFind) {
+    if (words2.length === 0) {
+        return null;
+    }
+    for (let i = 0; i < words2.length; i++) {
+        if (words2[i] === wordtoFind)
+        {
+            return true;
+        }
+    }
+    return false;
+}
